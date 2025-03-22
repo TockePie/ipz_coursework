15cb660bc2a0c450a720da5fec4df177e268fae7
Maxim <144056146+TockePie@users.noreply.github.com>
2025-03-22 12:26:12 +0200

    new colors and fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,44 @@
 import React, { ReactNode } from 'react'
+import clsx from 'clsx'
 import type { Metadata } from 'next'
-import { Geist, Geist_Mono } from 'next/font/google'
+import { Geist, Geist_Mono, Unbounded } from 'next/font/google'
+import localFont from 'next/font/local'
 
 import Navbar from './components/Navbar'
 import Providers from './providers'
 
 import './globals.css'
+
+const meila = localFont({
+  src: [
+    {
+      path: '../styles/fonts/MeilaDEMO-LineBold.woff',
+      weight: '700',
+      style: 'normal'
+    },
+    {
+      path: '../styles/fonts/MeilaDEMO-LineLight.woff',
+      weight: '300',
+      style: 'normal'
+    },
+    {
+      path: '../styles/fonts/MeilaDEMO-LineRegular.woff',
+      weight: '400',
+      style: 'normal'
+    },
+    {
+      path: '../styles/fonts/MeilaDEMO-Solid.woff',
+      weight: '900',
+      style: 'normal'
+    }
+  ],
+  variable: '--font-meila'
+})
+
+const unbounded = Unbounded({
+  variable: '--font-unbounded',
+  subsets: ['latin', 'cyrillic']
+})
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -30,7 +63,13 @@ const RootLayout = ({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={clsx(
+          geistSans.variable,
+          geistMono.variable,
+          meila.variable,
+          unbounded.variable,
+          'antialiased'
+        )}
       >
         <Providers>
           <Navbar />

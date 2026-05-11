5282fd56f65549ed0e5d1421680e7cafea82a7ea
Maxim <144056146+TockePie@users.noreply.github.com>
2026-05-11 13:16:17 +0300

    navbar

--- api/instance.ts
+++ api/instance.ts
@@ -1,18 +1,16 @@
 import axios from 'axios'
 
 const headers = { 'Content-Type': 'application/json' }
-const timeout = Number(process.env.AXIOS_REQUEST_TIMEOUT) ?? 10000
+const timeout = Number(process.env.NEXT_PUBLIC_AXIOS_REQUEST_TIMEOUT) ?? 10000
 
-const api = axios.create({
-  baseURL: process.env.API_URL,
+export const api = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   timeout,
   headers
 })
 
-const imgApi = axios.create({
-  baseURL: process.env.IMG_URL,
+export const imgApi = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_IMG_URL,
   timeout,
   headers
 })
-
-export { api, imgApi }
--- app/(main)/profile/page.tsx
+++ app/(main)/profile/page.tsx
@@ -1,30 +1,13 @@
-'use client'
+import { redirect } from 'next/navigation'
 
-import React, { useEffect, useState } from 'react'
-import { useRouter } from 'next/navigation'
-
-import useAuth from '@/hooks/api/use-auth'
+import isAuthenticated from '@/utils/is-authenticated'
 
 import ProfilePage from './components'
 
-const Profile = () => {
-  const [render, setRender] = useState(false)
-  const router = useRouter()
-  const { isAuthenticated } = useAuth()
-
-  useEffect(() => {
-    if (!isAuthenticated()) {
-      router.push('/login')
-      return
-    }
-    setRender(true)
-  }, [isAuthenticated, router])
-
-  if (!render) {
-    return null
+export default async function Profile() {
+  if (!isAuthenticated()) {
+    redirect('/login')
   }
 
   return <ProfilePage />
 }
-
-export default Profile
